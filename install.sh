5a320e09eba605e685f70b4930329f0a1ba521e8
Jardel Goncalves <jardelgoncalves1996@gmail.com>
2018-10-21 00:44:43 -0300

    atualizacao do script de instalacao

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ criando_tabelas(){
 }
 
 inserindo_valores(){
-	insert=`mysql -u root -p${1} beaver_db < sql/populando.sql ;echo $?`
+	insert=`mysql -u root -p${1} beaver_db < sql/populando.sql &> /dev/null ;echo $?`
 	if [ ${insert} = 0 ]; then
-                echo "usuario inserido"
+                echo "Usuario inserido"
         else
-                echo "usuario nao inserido"
+                echo "Usuario nao inserido"
         fi
 
 }
@@ -19,19 +19,21 @@ inserindo_valores(){
 add_user_e_db(){
 	funfou=`mysql -u root -p${1} < sql/config.sql &> /dev/null ;echo $?`
 	if [ ${funfou} = 0 ]; then
-		echo "Deu certo"
 		criando_tabelas
 		inserindo_valores ${1}
 	else
-		echo "Nao deu certo"
+		echo "Ocorreu um erro ao criar o usuario e o banco"
 	fi
 }
 
 
 while getopts "p:" OPTVAR
 do
-	add_user_e_db ${OPTARG}
-	echo "$OPTVAR $OPTARG"
+	if [ ${OPTVAR} = 'p' ]; then
+		add_user_e_db ${OPTARG}
+	else
+		echo "Necessario passar a senha de root do usuario mysql"
+	fi
 done
 
 
