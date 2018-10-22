9a4149fb8eb4acb7aac96c1e8fa46290b71253b2
JardelGoncalves <jardelgoncalves1996@gmail.com>
2018-10-22 00:48:15 -0300

    correcao do agent.py

--- monitoring/agent.py
+++ monitoring/agent.py
@@ -3,6 +3,7 @@ import requests
 import json
 import sys
 import datetime, time
+from requests.exceptions import ConnectionError
 
 try:
     mydb = mysql.connector.connect(
@@ -25,31 +26,34 @@ def monitoring_switches():
 
     for device in myresult:
         DPID=device[2]
-        r = requests.get("http://localhost:8080/stats/port/%i" % int(DPID,16))
-        if r.status_code == 200:
-            a = json.loads(r.text)
-            for elem in a[str(int(DPID,16))]:
-                if elem["port_no"] == "LOCAL":
-                    c = mydb.cursor()
-                    ultimo = "SELECT last_rx, last_tx FROM stats WHERE ip_dpid='%s' ORDER BY date DESC limit 1" %DPID
-                    c.execute(ultimo)
-                    consulta = c.fetchall()
-                    rx = 0
-                    tx = 0
-                    if len(consulta) > 0:
-                        rx = int(elem["rx_bytes"]) - consulta[0][0]
-                        tx = int(elem["tx_bytes"]) - consulta[0][1]
-                    else:
-                        rx = int(elem["rx_bytes"])
-                        tx = int(elem["tx_bytes"])
+        try: 
+            r = requests.get("http://localhost:8080/stats/port/%i" % int(DPID,16))
+            if r.status_code == 200:
+                a = json.loads(r.text)
+                for elem in a[str(int(DPID,16))]:
+                    if elem["port_no"] == "LOCAL":
+                        c = mydb.cursor()
+                        ultimo = "SELECT last_rx, last_tx FROM stats WHERE ip_dpid='%s' ORDER BY date DESC limit 1" %DPID
+                        c.execute(ultimo)
+                        consulta = c.fetchall()
+                        rx = 0
+                        tx = 0
+                        if len(consulta) > 0:
+                            rx = int(elem["rx_bytes"]) - consulta[0][0]
+                            tx = int(elem["tx_bytes"]) - consulta[0][1]
+                        else:
+                            rx = int(elem["rx_bytes"])
+                            tx = int(elem["tx_bytes"])
 
-                    insert = "INSERT INTO stats (ip_dpid, rx, tx, last_rx,last_tx, date) VALUES ('%s', %i, %i, %i, %i, '%s')" %(
-                                                            DPID, rx, tx, int(elem["rx_bytes"]),int(elem["tx_bytes"]), 
-                                                            datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-                    mycursor.execute(insert)
-                    mydb.commit()
-        else:
-            pass
+                        insert = "INSERT INTO stats (ip_dpid, rx, tx, last_rx,last_tx, date) VALUES ('%s', %i, %i, %i, %i, '%s')" %(
+                                                                DPID, rx, tx, int(elem["rx_bytes"]),int(elem["tx_bytes"]), 
+                                                                datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                        mycursor.execute(insert)
+                        mydb.commit()
+            else:
+                pass
+         except ConnectionError:
+             pass
 
     
 if __name__ == "__main__":
